177fec0f8e98a5009ee6c1beceb654397fc4a5e2
Marcin Mierzejewski <mierzejewski.marcin@gmail.com>
2023-03-07 15:13:04 +0100

    time marking

--- PNA2a.py
+++ PNA2a.py
@@ -16,8 +16,12 @@ m = 1000000000
 t1 = datetime.now()
 print('Start:', t1)
 print("Amount of prime numbers in range from 2 to", m, ":", len(pna(m)))
-print("The highest prime number in that range to", m, "is", max(pna(m)))
 t2 = datetime.now()
-print('Stop:', t2)
-a = t2 - t1
+at = t2 - t1
+print('Anount time:', at)
+print("The highest prime number in that range to", m, "is", max(pna(m)))
+t3 = datetime.now()
+tht = t3 - t2
+print('The highest time:', tht)
+a = t3 - t1
 print("Time of counting everything:", a)
